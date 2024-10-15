e449c94d62d331e58166295d9dfdcd47978bd231
Kanoe99 <kanoe.s99@gmail.com>
2024-10-15 09:03:15 +0500

    learn: electronmon watcher

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 const path = require('path');
-
 const {app, BrowserWindow} = require('electron');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform !== 'darwin';
 
 function createMainWindow(){
     const mainWindow = new BrowserWindow({
         title: 'Поздравительные Карточки',
-        width: 500,
+        width: isDev ? 1500 : 800,
         height: 800,
     });
+
+
+    if(isDev){
+        mainWindow.webContents.openDevTools();
+    }
 
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
 app.whenReady().then(()=>{
+
     createMainWindow();
+
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createMainWindow()
+      }
 });
+
+app.on('window-all-closed', () => {
+    if (!isMac) {
+      app.quit()
+    }
+  })
