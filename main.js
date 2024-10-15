d2d95cd05f76f7e83d7da07058c3529bbe07af00
Семён Максимович Гусев <145036030+Kanoe99@users.noreply.github.com>
2024-10-15 15:39:15 +0500

    fix: main files same as TraversyMedia, before hard reset

--- main.js
+++ main.js
@@ -1,35 +1,164 @@
 const path = require('path');
-const {app, BrowserWindow} = require('electron');
+const os = require('os');
+const fs = require('fs');
+const resizeImg = require('resize-img');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+
+process.env.NODE_ENV = 'production';
 
 const isDev = process.env.NODE_ENV !== 'production';
-const isMac = process.platform !== 'darwin';
+const isMac = process.platform === 'darwin';
 
-function createMainWindow(){
-    const mainWindow = new BrowserWindow({
-        title: 'Поздравительные Карточки',
-        width: isDev ? 1500 : 800,
-        height: 800,
-    });
+let mainWindow;
+let aboutWindow;
 
+// Main Window
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: isDev ? 1000 : 500,
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
 
-    if(isDev){
-        mainWindow.webContents.openDevTools();
-    }
+  // Show devtools automatically if in development
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
-    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+    // mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
+   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
-app.whenReady().then(()=>{
+// About Window
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    width: 300,
+    height: 300,
+    title: 'About Electron',
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+  });
 
-    createMainWindow();
+   aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+}
 
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createMainWindow()
-      }
+// When the app is ready, create the window
+app.on('ready', () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  // Remove variable from memory
+  mainWindow.on('closed', () => (mainWindow = null));
 });
 
-app.on('window-all-closed', () => {
-    if (!isMac) {
-      app.quit()
+// Menu template
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: 'fileMenu',
+  },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  // {
+  //   label: 'File',
+  //   submenu: [
+  //     {
+  //       label: 'Quit',
+  //       click: () => app.quit(),
+  //       accelerator: 'CmdOrCtrl+W',
+  //     },
+  //   ],
+  // },
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
+];
+
+// Respond to the resize image event
+ipcMain.on('image:resize', (e, options) => {
+  // console.log(options);
+  options.dest = path.join(os.homedir(), 'imageresizer');
+  resizeImage(options);
+});
+
+// Resize and save image
+async function resizeImage({ imgPath, height, width, dest }) {
+  try {
+    // console.log(imgPath, height, width, dest);
+
+    // Resize image
+    const newPath = await resizeImg(fs.readFileSync(imgPath), {
+      width: +width,
+      height: +height,
+    });
+
+    // Get filename
+    const filename = path.basename(imgPath);
+
+    // Create destination folder if it doesn't exist
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest);
     }
-  })
+
+    // Write the file to the destination folder
+    fs.writeFileSync(path.join(dest, filename), newPath);
+
+    // Send success to renderer
+    mainWindow.webContents.send('image:done');
+
+    // Open the folder in the file explorer
+    shell.openPath(dest);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (!isMac) app.quit();
+});
+
+// Open a window if none are open (macOS)
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+});
